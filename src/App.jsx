f145d85d0471ff054a5c8080eb164cf44a2683a0
PJ-clipper <pejavrajich@gmail.com>
2025-10-29 10:12:29 -0600

    Moved the products into own file

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,7 @@
 import SearchBar from "./components/SearchBar.jsx";
 import ProductTable from "./components/ProductTable.jsx";
 import { useState } from 'react';
-
-const PRODUCTS = [
-  { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
-  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
-  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
-  { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
-  { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
-  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
-];
+import { PRODUCTS } from "./consts/products.js";
 
 function FilterableProductTable() {
   const [filteredProducts, setFilteredProducts] = useState(PRODUCTS);
